2d6454d84ba0cae3b4081cfe5a0e872833b725ed
Yugal Kishore <yugalkishore59@gmail.com>
2023-09-12 03:12:43 +0530

    update

--- Portfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ Portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -5,10 +5,21 @@ import {images} from '../../constants'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      {/* why 2 underscores? this is BEM naming methodology. 
+      https://www.devbridge.com/articles/implementing-clean-css-bem-method/ */}
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt="logo" />
       </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item)=>(
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div>
+              <a href={`#${item}`}>{item}</a>
+            </div>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
