575eddf8d0c4008e554fdb3758d32bcc5e15e36b
Yugal Kishore <yugalkishore59@gmail.com>
2023-09-12 18:12:44 +0530

    finished navbar

--- Portfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ Portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Navbar.scss'
 import {images} from '../../constants'
+import {HiMenuAlt4, HiX } from 'react-icons/hi'
+import {motion} from 'framer-motion'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className='app__navbar'>
       {/* why 2 underscores? this is BEM naming methodology. 
@@ -14,12 +17,32 @@ const Navbar = () => {
       <ul className='app__navbar-links'>
         {['home', 'about', 'work', 'skills', 'contact'].map((item)=>(
           <li className='app__flex p-text' key={`link-${item}`}>
-            <div>
+            <div/>
               <a href={`#${item}`}>{item}</a>
-            </div>
           </li>
         ))}
       </ul>
+      <div className='app__navbar-menu'>
+          <HiMenuAlt4 onClick={()=> setToggle(true)}/>
+
+          {
+            toggle && (
+              <motion.div
+              whileInView={{x:[300,0]}} //this is going to take 300px
+              transition={{duration:0.85,ease: 'easeOut'}} //animation
+              >
+                <HiX onClick={()=> setToggle(false)}/>
+                <ul>
+                  {['home', 'about', 'work', 'skills', 'contact'].map((item)=>(
+                    <li key={item}>
+                      <a onClick={()=> setToggle(false)} href={`#${item}`}>{item}</a>
+                    </li>
+                  ))} 
+                </ul>
+              </motion.div>
+            )
+          }
+      </div>
     </nav>
   )
 }
