07fa73b87fb573baaa8f3cdfb364606d556b558b
Yugal Kishore <yugalkishore59@gmail.com>
2023-09-13 00:20:51 +0530

    working on work section

--- Portfolio/frontend_react/src/containers/Work/Work.jsx
+++ Portfolio/frontend_react/src/containers/Work/Work.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {AiFillEye, AiFillGitHub} from 'react-icons/ai'
+import {motion} from 'framer-motion'
+
+import {AppWrap} from '../../Wrapper'
+import {urlFor, client} from '../../client'
 
 import './Work.scss'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>
+        My creative <span>Portfolio</span> Section
+      </h2>
+    </>
   )
 }
 
